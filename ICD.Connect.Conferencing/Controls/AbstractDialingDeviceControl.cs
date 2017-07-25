f19f54d3c9bc16ece5f41640498e306816a2c709
Chris Cameron <chris.cameron@wedoresi.com>
2017-07-25 13:55:33 -0400

    Adding console commands and status to AbstractDialingDeviceControl

--- ICD.Connect.Conferencing/Controls/AbstractDialingDeviceControl.cs
+++ ICD.Connect.Conferencing/Controls/AbstractDialingDeviceControl.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using ICD.Common.EventArguments;
 using ICD.Common.Properties;
 using ICD.Common.Utils.Extensions;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
 using ICD.Connect.Conferencing.ConferenceSources;
 using ICD.Connect.Conferencing.EventArguments;
 using ICD.Connect.Devices;
@@ -147,6 +149,47 @@ namespace ICD.Connect.Conferencing.Controls
 		/// </summary>
 		/// <param name="enabled"></param>
 		public abstract void SetPrivacyMute(bool enabled);
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new GenericConsoleCommand<string>("Dial", "Dial <NUMBER>", s => Dial(s));
+			yield return new GenericConsoleCommand<bool>("SetDoNotDisturb", "SetDoNotDisturb <true/false>", b => SetDoNotDisturb(b));
+			yield return new GenericConsoleCommand<bool>("SetAutoAnswer", "SetAutoAnswer <true/false>", b => SetAutoAnswer(b));
+			yield return new GenericConsoleCommand<bool>("SetPrivacyMute", "SetPrivacyMute <true/false>", b => SetPrivacyMute(b));
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			addRow("AutoAnswer", m_AutoAnswer);
+			addRow("PrivacyMuted", m_PrivacyMuted);
+			addRow("DoNotDisturb", m_DoNotDisturb);
+		}
 
 		#endregion
 	}
