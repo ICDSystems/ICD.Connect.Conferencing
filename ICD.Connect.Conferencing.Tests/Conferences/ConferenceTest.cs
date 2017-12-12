f6dbc3a46424c0594c47886e6ef120a0a5a9b2ad
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-12 09:56:00 -0500

    Test stubs

---
+++ ICD.Connect.Conferencing.Tests/Conferences/ConferenceTest.cs
@@ -0,0 +1,71 @@
+﻿using ICD.Common.Properties;
+using NUnit.Framework;
+
+namespace ICD.Connect.Conferencing.Tests.Conferences
+{
+	[PublicAPI]
+	public sealed class ConferenceTest
+	{
+		#region Properties
+
+		[Test]
+		public void StatusTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void StartTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void EndTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void SourcesCountTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void OnlineSourcesCountTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		#endregion
+
+		#region Methods
+
+		[Test]
+		public void ClearTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void AddSourceTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void RemoveSourceTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetSourcesTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		#endregion
+	}
+}
