dd06c2693686737633e210e3d750069c23dd826e
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-05 10:35:16 -0500

    Updating copyright info

--- ICD.Connect.Conferencing.Cisco/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing.Cisco/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("ICD.Connect.Conferencing.Cisco")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing.Cisco")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("3.0.0.*")]
