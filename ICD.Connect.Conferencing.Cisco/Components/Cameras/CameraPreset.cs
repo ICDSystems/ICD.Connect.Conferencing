9ed9a19b89638dea2157cd6d148917dead1c4b04
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-10 13:01:41 -0500

    Implementing equality for CameraPreset

--- ICD.Connect.Conferencing.Cisco/Components/Cameras/CameraPreset.cs
+++ ICD.Connect.Conferencing.Cisco/Components/Cameras/CameraPreset.cs
@@ -100,5 +100,61 @@ namespace ICD.Connect.Conferencing.Cisco.Components.Cameras
 		}
 
 		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Implementing default equality.
+		/// </summary>
+		/// <param name="s1"></param>
+		/// <param name="s2"></param>
+		/// <returns></returns>
+		public static bool operator ==(CameraPreset s1, CameraPreset s2)
+		{
+			return s1.Equals(s2);
+		}
+
+		/// <summary>
+		/// Implementing default inequality.
+		/// </summary>
+		/// <param name="s1"></param>
+		/// <param name="s2"></param>
+		/// <returns></returns>
+		public static bool operator !=(CameraPreset s1, CameraPreset s2)
+		{
+			return !(s1 == s2);
+		}
+
+		/// <summary>
+		/// Returns true if this instance is equal to the given object.
+		/// </summary>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		public override bool Equals(object other)
+		{
+			if (other == null || GetType() != other.GetType())
+				return false;
+
+			return GetHashCode() == ((CameraPreset)other).GetHashCode();
+		}
+
+		/// <summary>
+		/// Gets the hashcode for this instance.
+		/// </summary>
+		/// <returns></returns>
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hash = 17;
+				hash = hash * 23 + m_PresetId;
+				hash = hash * 23 + m_CameraId;
+				hash = hash * 23 + m_ListPosition;
+				hash = hash * 23 + (m_Name == null ? 0 : m_Name.GetHashCode());
+				return hash;
+			}
+		}
+
+		#endregion
 	}
 }
