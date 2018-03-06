aae7befbc1f0144cd21082db410003c18e441d0a
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-06 14:41:54 -0500

    Tidying

--- ICD.Connect.Conferencing.Cisco/eCodecInputType.cs
+++ ICD.Connect.Conferencing.Cisco/eCodecInputType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ICD.Connect.Conferencing.Cisco
+﻿namespace ICD.Connect.Conferencing.Cisco
 {
     public enum eCodecInputType
     {
