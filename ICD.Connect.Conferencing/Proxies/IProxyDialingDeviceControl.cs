8e922170449250a3475a03b063b14297d0c4041c
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-30 15:18:29 -0400

    Initial commit of dialing control proxy

---
+++ ICD.Connect.Conferencing/Proxies/IProxyDialingDeviceControl.cs
@@ -0,0 +1,9 @@
+﻿using ICD.Connect.Conferencing.Controls;
+using ICD.Connect.Devices.Proxies.Controls;
+
+namespace ICD.Connect.Conferencing.Proxies
+{
+	public interface IProxyDialingDeviceControl : IProxyDeviceControl, IDialingDeviceControl
+	{
+	}
+}
