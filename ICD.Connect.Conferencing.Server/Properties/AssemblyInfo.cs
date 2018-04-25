26a188324336aaeae15b5c1b002190a665baa940
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-04-25 17:36:47 -0400

    feat: more project files

---
+++ ICD.Connect.Conferencing.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Conferencing.Server")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ICD.Connect.Conferencing.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
