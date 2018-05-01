effb70005bedf8cb9e06439f3ca93c4a80f0ff82
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-01 15:04:56 -0400

    chore: Initial commit of Polycom project

---
+++ ICD.Connect.Conferencing.Polycom/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Conferencing.Polycom")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Conferencing.Polycom")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyVersion("5.1.0.0")]
