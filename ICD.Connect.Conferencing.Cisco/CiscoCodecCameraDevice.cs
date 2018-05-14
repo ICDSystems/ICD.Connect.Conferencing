d57b90909317b7d45f38a5478cd9d28e02b337ef
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-14 16:45:25 -0400

    refactor: Using standardized logging

--- ICD.Connect.Conferencing.Cisco/CiscoCodecCameraDevice.cs
+++ ICD.Connect.Conferencing.Cisco/CiscoCodecCameraDevice.cs
@@ -136,7 +136,7 @@ namespace ICD.Connect.Conferencing.Cisco
 
 			if (presetId < 1 || presetId > MaxPresets)
 			{
-				Logger.AddEntry(eSeverity.Warning, "Camera preset must be between 1 and {0}, preset was not activated.", MaxPresets);
+				Log(eSeverity.Warning, "Camera preset must be between 1 and {0}, preset was not activated.", MaxPresets);
 				return;
 			}
 
@@ -150,7 +150,7 @@ namespace ICD.Connect.Conferencing.Cisco
 
 			if (presetId < 1 || presetId > MaxPresets)
 			{
-				Logger.AddEntry(eSeverity.Warning, "Camera preset must be between 1 and {0}, preset was not stored.", MaxPresets);
+				Log(eSeverity.Warning, "Camera preset must be between 1 and {0}, preset was not stored.", MaxPresets);
 				return;
 			}
 
@@ -194,7 +194,7 @@ namespace ICD.Connect.Conferencing.Cisco
 
 			if (settings.CameraId == null)
 			{
-				Logger.AddEntry(eSeverity.Error, "No camera id set for camera: {0}", Name);
+				Log(eSeverity.Error, "No camera id set for camera: {0}", Name);
 				return;
 			}
 
