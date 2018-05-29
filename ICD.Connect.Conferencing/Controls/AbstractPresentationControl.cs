e0878ce3cef9874be774a086321e115ed383abc9
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-29 18:06:29 -0400

    feat: Begin implementing presentation control

---
+++ ICD.Connect.Conferencing/Controls/AbstractPresentationControl.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Devices;
+using ICD.Connect.Devices.Controls;
+
+namespace ICD.Connect.Conferencing.Controls
+{
+	public abstract class AbstractPresentationControl<TDevice> : AbstractDeviceControl<TDevice>, IPresentationControl
+		where TDevice : IDeviceBase
+	{
+	}
+}
---
+++ ICD.Connect.Conferencing/Controls/IPresentationControl.cs
@@ -0,0 +1,13 @@
+﻿using ICD.Connect.Devices.Controls;
+
+namespace ICD.Connect.Conferencing.Controls
+{
+	public interface IPresentationControl : IDeviceControl
+	{
+		/// <summary>
+		/// Starts presenting the source at the given input address.
+		/// </summary>
+		/// <param name="input"></param>
+		void StartPresentation(int input);
+	}
+}
