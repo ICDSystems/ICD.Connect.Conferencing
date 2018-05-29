221c09ad59258a715b598feaf87a44cf6952875d
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-29 16:23:35 -0400

    feat: Initial commit of conferencing device interfaces and abstractions

---
+++ ICD.Connect.Conferencing/Devices/IConferencingDeviceSettings.cs
@@ -0,0 +1,12 @@
+﻿using ICD.Connect.Devices;
+
+namespace ICD.Connect.Conferencing.Devices
+{
+	public interface IConferencingDeviceSettings : IDeviceSettings
+	{
+		eCodecInputType Input1CodecInputType { get; set; }
+		eCodecInputType Input2CodecInputType { get; set; }
+		eCodecInputType Input3CodecInputType { get; set; }
+		eCodecInputType Input4CodecInputType { get; set; }
+	}
+}
