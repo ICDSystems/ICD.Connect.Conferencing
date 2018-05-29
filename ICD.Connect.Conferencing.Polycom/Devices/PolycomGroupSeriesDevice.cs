d362dfd9e6c97fd3f7daadd6aa1b2d8fa6f6abee
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-29 14:27:33 -0400

    feat: Initial commit of Polycom Group Series device and settings

---
+++ ICD.Connect.Conferencing.Polycom/Devices/PolycomGroupSeriesDevice.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Devices;
+using ICD.Connect.Devices.EventArguments;
+using ICD.Connect.Protocol.Extensions;
+using ICD.Connect.Protocol.Heartbeat;
+using ICD.Connect.Protocol.Ports;
+using ICD.Connect.Protocol.Ports.ComPort;
+using ICD.Connect.Protocol.SerialBuffers;
+using ICD.Connect.Settings.Core;
+
+namespace ICD.Connect.Conferencing.Polycom.Devices
+{
+	public sealed class PolycomGroupSeriesDevice : AbstractDevice<PolycomGroupSeriesSettings>, IConnectable
+	{
+		/// <summary>
+		/// End of line string.
+		/// </summary>
+		private const string END_OF_LINE = "\x0D\x0A";
+
+		/// <summary>
+		/// Raised when the class initializes.
+		/// </summary>
+		public event EventHandler<BoolEventArgs> OnInitializedChanged;
+
+		/// <summary>
+		/// Raised when the device becomes connected or disconnected.
+		/// </summary>
+		public event EventHandler<BoolEventArgs> OnConnectedStateChanged;
+
+		private readonly ISerialBuffer m_SerialBuffer;
+
+		private bool m_Initialized;
+		private bool m_IsConnected;
+		private ISerialPort m_Port;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the heartbeat instance that is enforcing the connection state.
+		/// </summary>
+		public Heartbeat Heartbeat { get; private set; }
+
+		/// <summary>
+		/// Device Initialized Status.
+		/// </summary>
+		public bool Initialized
+		{
+			get { return m_Initialized; }
+			private set
+			{
+				if (value == m_Initialized)
+					return;
+
+				m_Initialized = value;
+
+				OnInitializedChanged.Raise(this, new BoolEventArgs(m_Initialized));
+			}
+		}
+
+		/// <summary>
+		/// Returns true when the codec is connected.
+		/// </summary>
+		public bool IsConnected
+		{
+			get { return m_IsConnected; }
+			private set
+			{
+				if (value == m_IsConnected)
+					return;
+
+				m_IsConnected = value;
+
+				OnConnectedStateChanged.Raise(this, new BoolEventArgs(m_IsConnected));
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public PolycomGroupSeriesDevice()
+		{
+			Heartbeat = new Heartbeat(this);
+
+			m_SerialBuffer = new XmlSerialBuffer();
+			Subscribe(m_SerialBuffer);
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnInitializedChanged = null;
+			OnConnectedStateChanged = null;
+
+			Heartbeat.Dispose();
+
+			Unsubscribe(m_SerialBuffer);
+			Unsubscribe(m_Port);
+
+			base.DisposeFinal(disposing);
+		}
+
+		/// <summary>
+		/// Sets the port for communicating with the device.
+		/// </summary>
+		/// <param name="port"></param>
+		[PublicAPI]
+		public void SetPort(ISerialPort port)
+		{
+			if (port == m_Port)
+				return;
+
+			if (port is IComPort)
+				ConfigureComPort(port as IComPort);
+
+			if (m_Port != null)
+				Disconnect();
+
+			Unsubscribe(m_Port);
+			m_Port = port;
+			Subscribe(m_Port);
+
+			if (m_Port != null)
+				Heartbeat.StartMonitoring();
+
+			UpdateCachedOnlineStatus();
+		}
+
+		/// <summary>
+		/// Configures a com port for communication with the hardware.
+		/// </summary>
+		/// <param name="port"></param>
+		[PublicAPI]
+		public static void ConfigureComPort(IComPort port)
+		{
+			port.SetComPortSpec(eComBaudRates.ComspecBaudRate115200,
+			                    eComDataBits.ComspecDataBits8,
+			                    eComParityType.ComspecParityNone,
+			                    eComStopBits.ComspecStopBits1,
+			                    eComProtocolType.ComspecProtocolRS232,
+			                    eComHardwareHandshakeType.ComspecHardwareHandshakeNone,
+			                    eComSoftwareHandshakeType.ComspecSoftwareHandshakeNone,
+			                    false);
+		}
+
+		/// <summary>
+		/// Connect to the codec.
+		/// </summary>
+		[PublicAPI]
+		public void Connect()
+		{
+			if (m_Port == null)
+			{
+				Log(eSeverity.Critical, "Unable to connect, port is null");
+				return;
+			}
+
+			m_Port.Connect();
+			IsConnected = m_Port.IsConnected;
+
+			if (IsConnected)
+				Initialize();
+		}
+
+		/// <summary>
+		/// Disconnect from the codec.
+		/// </summary>
+		[PublicAPI]
+		public void Disconnect()
+		{
+			if (m_Port == null)
+			{
+				Log(eSeverity.Critical, "Unable to disconnect, port is null");
+				return;
+			}
+
+			m_Port.Disconnect();
+			IsConnected = m_Port.IsConnected;
+		}
+
+		/// <summary>
+		/// Send command.
+		/// </summary>
+		/// <param name="command"></param>
+		public void SendCommand(string command)
+		{
+			SendCommand(command, null);
+		}
+
+		/// <summary>
+		/// Send command.
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="args"></param>
+		public void SendCommand(string command, params object[] args)
+		{
+			if (args != null)
+				command = string.Format(command, args);
+
+			if (m_Port == null)
+			{
+				Log(eSeverity.Error, "Unable to communicate with device - port is null");
+				return;
+			}
+
+			if (!IsConnected)
+			{
+				Log(eSeverity.Warning, "Device is disconnected, attempting reconnect");
+				Connect();
+			}
+
+			if (!IsConnected)
+			{
+				Log(eSeverity.Critical, "Unable to communicate with device");
+				return;
+			}
+
+			m_Port.Send(command + END_OF_LINE);
+		}
+
+		/// <summary>
+		/// Sends commands.
+		/// </summary>
+		/// <param name="commands"></param>
+		[PublicAPI]
+		public void SendCommands(params string[] commands)
+		{
+			if (commands == null)
+				throw new ArgumentNullException("commands");
+
+			foreach (string command in commands)
+				SendCommand(command);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+			return m_Port != null && m_Port.IsOnline;
+		}
+
+		/// <summary>
+		/// Initialize the device.
+		/// </summary>
+		private void Initialize()
+		{
+			Initialized = true;
+		}
+
+		#endregion
+
+		#region Port Callbacks
+
+		/// <summary>
+		/// Subscribes to the port events.
+		/// </summary>
+		/// <param name="port"></param>
+		private void Subscribe(ISerialPort port)
+		{
+			if (port == null)
+				return;
+
+			port.OnSerialDataReceived += PortOnSerialDataReceived;
+			port.OnConnectedStateChanged += PortOnConnectionStatusChanged;
+			port.OnIsOnlineStateChanged += PortOnIsOnlineStateChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the port events.
+		/// </summary>
+		/// <param name="port"></param>
+		private void Unsubscribe(ISerialPort port)
+		{
+			if (port == null)
+				return;
+
+			port.OnSerialDataReceived -= PortOnSerialDataReceived;
+			port.OnConnectedStateChanged -= PortOnConnectionStatusChanged;
+			port.OnIsOnlineStateChanged -= PortOnIsOnlineStateChanged;
+		}
+
+		/// <summary>
+		/// Called when serial data is recieved from the port.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void PortOnSerialDataReceived(object sender, StringEventArgs args)
+		{
+			m_SerialBuffer.Enqueue(args.Data);
+		}
+
+		/// <summary>
+		/// Called when the port connection status changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void PortOnConnectionStatusChanged(object sender, BoolEventArgs args)
+		{
+			m_SerialBuffer.Clear();
+
+			IsConnected = args.Data;
+
+			if (IsConnected)
+				Initialize();
+			else
+			{
+				Log(eSeverity.Critical, "Lost connection");
+				Initialized = false;
+			}
+		}
+
+		/// <summary>
+		/// Called when the port online status changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void PortOnIsOnlineStateChanged(object sender, DeviceBaseOnlineStateApiEventArgs args)
+		{
+			UpdateCachedOnlineStatus();
+		}
+
+		#endregion
+
+		#region Buffer Callbacks
+
+		/// <summary>
+		/// Subscribes to the buffer events.
+		/// </summary>
+		/// <param name="buffer"></param>
+		private void Subscribe(ISerialBuffer buffer)
+		{
+			buffer.OnCompletedSerial += SerialBufferCompletedSerial;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the buffer events.
+		/// </summary>
+		/// <param name="buffer"></param>
+		private void Unsubscribe(ISerialBuffer buffer)
+		{
+			buffer.OnCompletedSerial -= SerialBufferCompletedSerial;
+		}
+
+		/// <summary>
+		/// Called when the buffer completes a string.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void SerialBufferCompletedSerial(object sender, StringEventArgs args)
+		{
+		}
+
+		#endregion
+
+		#region Settings
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(PolycomGroupSeriesSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.Port = m_Port == null ? (int?)null : m_Port.Id;
+		}
+
+		/// <summary>
+		/// Override to clear the instance settings.
+		/// </summary>
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			SetPort(null);
+		}
+
+		/// <summary>
+		/// Override to apply settings to the instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		protected override void ApplySettingsFinal(PolycomGroupSeriesSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			ISerialPort port = null;
+
+			if (settings.Port != null)
+			{
+				port = factory.GetPortById((int)settings.Port) as ISerialPort;
+				if (port == null)
+					Log(eSeverity.Error, "No serial port with id {0}", settings.Port);
+			}
+
+			SetPort(port);
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			addRow("Connected", IsConnected);
+			addRow("Initialized", Initialized);
+		}
+
+		#endregion
+	}
+}
