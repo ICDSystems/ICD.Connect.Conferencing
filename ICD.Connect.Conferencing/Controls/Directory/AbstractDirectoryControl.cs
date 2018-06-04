065aea971fb84bc1d7433031acb17f7bb6bcaf06
Chris Cameron <chris.cameron@wedoresi.com>
2018-06-04 12:04:03 -0400

    feat: Adding console to directory control

---
+++ ICD.Connect.Conferencing/Controls/Directory/AbstractDirectoryControl.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Conferencing.Directory.Tree;
+using ICD.Connect.Devices;
+using ICD.Connect.Devices.Controls;
+
+namespace ICD.Connect.Conferencing.Controls.Directory
+{
+	public abstract class AbstractDirectoryControl<TParent> : AbstractDeviceControl<TParent>, IDirectoryControl
+		where TParent : IDeviceBase
+	{
+		/// <summary>
+		/// Raised when the directory tree is cleared.
+		/// </summary>
+		public abstract event EventHandler OnCleared;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		protected AbstractDirectoryControl(TParent parent, int id)
+			: base(parent, id)
+		{
+		}
+
+		#region Methods
+
+		/// <summary>
+		/// Gets the root folder for the directory.
+		/// </summary>
+		/// <returns></returns>
+		public abstract IDirectoryFolder GetRoot();
+
+		/// <summary>
+		/// Clears the cached directory for repopulation.
+		/// </summary>
+		public abstract void Clear();
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			foreach (IConsoleCommand command in DirectoryControlConsole.GetConsoleCommands(this))
+				yield return command;
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			DirectoryControlConsole.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleNodeBase> GetConsoleNodes()
+		{
+			foreach (IConsoleNodeBase node in GetBaseConsoleNodes())
+				yield return node;
+
+			foreach (IConsoleNodeBase node in DirectoryControlConsole.GetConsoleNodes(this))
+				yield return node;
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleNodeBase> GetBaseConsoleNodes()
+		{
+			return base.GetConsoleNodes();
+		}
+
+		#endregion
+	}
+}
