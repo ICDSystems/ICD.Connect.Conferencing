12cde35c7606009c4038dc6e9811ecde3c168db2
Chris Cameron <chris.cameron@wedoresi.com>
2018-06-04 12:04:19 -0400

    feat: Begin implementing proxy for directory control

--- ICD.Connect.Conferencing/Controls/Directory/IDirectoryControl.cs
+++ ICD.Connect.Conferencing/Controls/Directory/IDirectoryControl.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using ICD.Connect.API.Attributes;
 using ICD.Connect.Conferencing.Directory.Tree;
+using ICD.Connect.Conferencing.Proxies.Controls.Directory;
 using ICD.Connect.Devices.Controls;
 
 namespace ICD.Connect.Conferencing.Controls.Directory
 {
+	[ApiClass(typeof(ProxyDirectoryControl), typeof(IDeviceControl))]
 	public interface IDirectoryControl : IDeviceControl
 	{
 		/// <summary>
 		/// Raised when the directory tree is cleared.
 		/// </summary>
+		[ApiEvent(DirectoryControlApi.EVENT_CLEARED, DirectoryControlApi.HELP_EVENT_CLEARED)]
 		event EventHandler OnCleared;
 
 		/// <summary>
@@ -20,6 +24,7 @@ namespace ICD.Connect.Conferencing.Controls.Directory
 		/// <summary>
 		/// Clears the cached directory for repopulation.
 		/// </summary>
+		[ApiMethod(DirectoryControlApi.METHOD_CLEAR, DirectoryControlApi.HELP_METHOD_CLEAR)]
 		void Clear();
 	}
 }
---
+++ ICD.Connect.Conferencing/Proxies/Controls/Directory/ProxyDirectoryControl.cs
@@ -0,0 +1,17 @@
+﻿using ICD.Connect.Devices.Proxies.Devices;
+
+namespace ICD.Connect.Conferencing.Proxies.Controls.Directory
+{
+	public sealed class ProxyDirectoryControl : AbstractProxyDirectoryControl
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		public ProxyDirectoryControl(IProxyDeviceBase parent, int id)
+			: base(parent, id)
+		{
+		}
+	}
+}
