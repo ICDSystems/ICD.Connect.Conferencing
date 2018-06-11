76049ebcf30bc9a74b422c93d63066f6cf99b9b5
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-06-11 11:16:56 -0400

    fix: handle null originator better

--- ICD.Connect.Conferencing.Server/SimplShims/InterpretationServerShim.cs
+++ ICD.Connect.Conferencing.Server/SimplShims/InterpretationServerShim.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Properties;
 using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.Conferencing.Server.Devices.Server;
 using ICD.Connect.Settings.SPlusShims;
 
@@ -10,30 +11,53 @@ namespace ICD.Connect.Conferencing.Server.SimplShims
 {
 	public sealed class InterpretationServerShim : AbstractSPlusOriginatorShim<IInterpretationServerDevice>
 	{
+		[PublicAPI("S+")]
 		public event EventHandler<InterpretationStateEventArgs> OnInterpretationStateChanged;
+
+		[PublicAPI("S+")]
 		public event EventHandler<InterpretationRoomInfoArgs> OnRoomAdded;
 
 		[PublicAPI("S+")]
 		public void BeginInterpretation(ushort roomId, ushort boothId)
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot begin interpretation.");
+				return;
+			}
 			Originator.BeginInterpretation(roomId, boothId);
 		}
 
 		[PublicAPI("S+")]
 		public void EndInterpretation(ushort roomId, ushort boothId)
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot end interpretation.");
+				return;
+			}
 			Originator.EndInterpretation(roomId, boothId);
 		}
 
 		[PublicAPI("S+")]
 		public ushort[] GetAvailableBoothIds()
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot get booth ids.");
+				return new ushort[0];
+			}
 			return Originator.GetAvailableBoothIds().ToArray();
 		}
 
 		[PublicAPI("S+")]
 		public ushort[] GetAvailableRoomIds()
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot get room ids.");
+				return new ushort[0];
+			}
 			int[] available = Originator.GetAvailableRoomIds().ToArray();
 			IEnumerable<ushort> availableAsUShorts = available.Where(value => value >= ushort.MinValue && value <= ushort.MaxValue)
 															  .Select(value => (ushort)value);
@@ -44,24 +68,44 @@ namespace ICD.Connect.Conferencing.Server.SimplShims
 		[PublicAPI("S+")]
 		public string GetRoomName(int roomId)
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot get room name.");
+				return string.Empty;
+			}
 			return Originator.GetRoomName(roomId);
 		}
 
 		[PublicAPI("S+")]
 		public string GetRoomPrefix(int roomId)
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot get room prefix.");
+				return string.Empty;
+			}
 			return Originator.GetRoomPrefix(roomId);
 		}
 
 		[PublicAPI("S+")]
 		public ushort GetBoothId(int roomId)
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot get booth id.");
+				return 0;
+			}
 			return Originator.GetBoothId(roomId);
 		}
 
 		[PublicAPI("S+")]
 		public ushort GetRoomExists(int roomId)
 		{
+			if (Originator == null)
+			{
+				Log(eSeverity.Error, "Originator Is Null, cannot get room existance status.");
+				return 0;
+			}
 			return Originator.GetRoomExists(roomId);
 		}
 
@@ -83,10 +127,10 @@ namespace ICD.Connect.Conferencing.Server.SimplShims
 		/// <param name="originator"></param>
 		protected override void Unsubscribe(IInterpretationServerDevice originator)
 		{
-			base.Subscribe(originator);
-
 			originator.OnInterpretationStateChanged -= OriginatorOnInterpretationStateChanged;
 			originator.OnRoomAdded -= OriginatorOnRoomAdded;
+
+			base.Unsubscribe(originator);
 		}
 
 		private void OriginatorOnInterpretationStateChanged(object sender, InterpretationStateEventArgs args)
