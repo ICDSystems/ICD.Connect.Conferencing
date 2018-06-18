54b3ed9bf6c81be5c7d32c7b67c6e8a07918f52c
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-06-18 11:39:52 -0400

    feat: allow clients to override the room name and prefix in their settings

--- ICD.Connect.Conferencing.Server/Devices/Client/IClientInterpretationDevice.cs
+++ ICD.Connect.Conferencing.Server/Devices/Client/IClientInterpretationDevice.cs
@@ -4,7 +4,10 @@ namespace ICD.Connect.Conferencing.Server.Devices.Client
 {
 	public interface IClientInterpretationDevice : IInterpretationDevice
 	{
-		string RoomName { get; set; }
-		string RoomPrefix { get; set; }
+		string RoomName { get; }
+		string RoomPrefix { get; }
+
+		void SetRoomName(string name);
+		void SetRoomPrefix(string prefix);
 	}
 }
