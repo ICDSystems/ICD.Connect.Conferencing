62af200fee5d07be2c1c2dd83c243417a9826dda
Chris Cameron <chris.cameron@wedoresi.com>
2018-07-11 09:24:40 -0400

    feat: Change polycom directory names to "Search by letter:"

--- ICD.Connect.Conferencing.Polycom/Devices/Codec/Components/Addressbook/AddressbookComponent.cs
+++ ICD.Connect.Conferencing.Polycom/Devices/Codec/Components/Addressbook/AddressbookComponent.cs
@@ -285,7 +285,7 @@ namespace ICD.Connect.Conferencing.Polycom.Devices.Codec.Components.Addressbook
 
 			if (folder == null)
 			{
-				string folderName = string.Format("Letter: {0}", letter);
+				string folderName = string.Format("Search by letter: {0}", letter);
 				folder = new DirectoryFolder(folderName);
 
 				root.AddFolder(folder);
