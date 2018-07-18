3346b87f6490591951d78f74ab2c64a74d5b2415
Chris Cameron <chris.cameron@wedoresi.com>
2018-07-18 14:11:29 -0400

    add onsourceremoved to dialers
    # Conflicts:
    #	ICD.Connect.Conferencing.Mock/IMockConferencingDevice.cs
    #	ICD.Connect.Conferencing.Mock/MockConferencingDevice.cs
    #	ICD.Connect.Conferencing.Mock/MockDialingDeviceControl.cs
    #	ICD.Connect.Conferencing.Server/ConferencingClientDevice.cs
    #	ICD.Connect.Conferencing/Controls/IDialingDeviceControl.cs

--- ICD.Connect.Conferencing.Cisco/Controls/CiscoDialingDeviceControl.cs
+++ ICD.Connect.Conferencing.Cisco/Controls/CiscoDialingDeviceControl.cs
@@ -15,6 +15,7 @@ namespace ICD.Connect.Conferencing.Cisco.Controls
 		/// Called when a source is added to the dialing component.
 		/// </summary>
 		public override event EventHandler<ConferenceSourceEventArgs> OnSourceAdded;
+		public override event EventHandler<ConferenceSourceEventArgs> OnSourceRemoved;
 
 		private readonly DialingComponent m_Component;
 
@@ -47,6 +48,7 @@ namespace ICD.Connect.Conferencing.Cisco.Controls
 		protected override void DisposeFinal(bool disposing)
 		{
 			OnSourceAdded = null;
+			OnSourceRemoved = null;
 
 			base.DisposeFinal(disposing);
 
@@ -117,18 +119,25 @@ namespace ICD.Connect.Conferencing.Cisco.Controls
 		private void Subscribe(DialingComponent component)
 		{
 			component.OnSourceAdded += ComponentOnSourceAdded;
+			component.OnSourceRemoved += ComponentOnSourceRemoved;
 			component.OnPrivacyMuteChanged += ComponentOnPrivacyMuteChanged;
 		}
 
 		private void Unsubscribe(DialingComponent component)
 		{
 			component.OnSourceAdded -= ComponentOnSourceAdded;
+			component.OnSourceRemoved -= ComponentOnSourceRemoved;
 			component.OnPrivacyMuteChanged -= ComponentOnPrivacyMuteChanged;
 		}
 
 		private void ComponentOnSourceAdded(object sender, ConferenceSourceEventArgs args)
 		{
 			OnSourceAdded.Raise(this, new ConferenceSourceEventArgs(args.Data));
+		}
+
+		private void ComponentOnSourceRemoved(object sender, ConferenceSourceEventArgs args)
+		{
+			OnSourceRemoved.Raise(this, new ConferenceSourceEventArgs(args.Data));
 		}
 
 		private void ComponentOnPrivacyMuteChanged(object sender, BoolEventArgs args)
