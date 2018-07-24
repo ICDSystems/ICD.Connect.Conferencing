a1599e414ea40be9d63fb11b8367335f172cce16
Drew Tingen <drew.tingen@icdsystems.com>
2018-07-24 21:32:48 +0900

    fix: removing redundant Register call

--- ICD.Connect.Conferencing.Server/Devices/Client/InterpretationClientDevice.cs
+++ ICD.Connect.Conferencing.Server/Devices/Client/InterpretationClientDevice.cs
@@ -611,10 +611,7 @@ namespace ICD.Connect.Conferencing.Server.Devices.Client
 		    IsConnected = m_ConnectionStateManager != null && m_ConnectionStateManager.IsConnected;
 
 		    if (IsConnected)
-		    {
-				Register();
 			    return;
-		    }
 
 		    IsInterpretationActive = false;
 		    ClearSources();
