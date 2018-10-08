3fc4696c79d058aca8e90d39f60ed4a9fc36b7f7
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-08 09:37:23 -0400

    feat: ConferencingBookingUtils slightly better at determining meeting type

--- ICD.Connect.Conferencing/Utils/ConferencingBookingUtils.cs
+++ ICD.Connect.Conferencing/Utils/ConferencingBookingUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Properties;
+using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Calendaring;
 using ICD.Connect.Calendaring.Booking;
@@ -10,7 +11,7 @@ using ICD.Connect.Conferencing.EventArguments;
 
 namespace ICD.Connect.Conferencing.Utils
 {
-    public static class ConferencingBookingUtils
+	public static class ConferencingBookingUtils
 	{
 		/// <summary>
 		/// Gets the best dialer for the booking based on what is supported by the given dialers.
@@ -20,7 +21,8 @@ namespace ICD.Connect.Conferencing.Utils
 		/// <param name="number"></param>
 		/// <returns></returns>
 		[CanBeNull]
-		public static IDialingDeviceControl GetBestDialer(IBooking booking, IEnumerable<IDialingDeviceControl> dialers, out IBookingNumber number)
+		public static IDialingDeviceControl GetBestDialer(IBooking booking, IEnumerable<IDialingDeviceControl> dialers,
+		                                                  out IBookingNumber number)
 		{
 			if (booking == null)
 				throw new ArgumentNullException("booking");
@@ -56,26 +58,33 @@ namespace ICD.Connect.Conferencing.Utils
 			if (dialers == null)
 				throw new ArgumentNullException("dialers");
 
-			// Build map of dialer to best number
-			Dictionary<IDialingDeviceControl, IBookingNumber> map = dialers.ToDictionary(d => d, d => GetBestNumber(d, booking));
-
-			IDialingDeviceControl preferredDialer = map.Keys
-			                                           .Where(d => map.GetDefault(d) != null)
-			                                           .OrderByDescending(d => d.CanDial(map[d]))
-			                                           .ThenByDescending(d => d.Supports)
-			                                           .FirstOrDefault();
+			IBookingNumber bookingNumber;
+			IDialingDeviceControl preferredDialer = GetBestDialer(booking, dialers, out bookingNumber);
 
 			if (preferredDialer == null)
 				return eMeetingType.Presentation;
 
-			if (preferredDialer.Supports.HasFlag(eConferenceSourceType.Video))
-			{
-				eMeetingType conferenceSourceType = ToConferenceSourceType(map.GetDefault(preferredDialer).Protocol);
-				return eMeetingType.VideoConference == conferenceSourceType ? eMeetingType.VideoConference : conferenceSourceType;
-			}
+			eMeetingType meetingType = bookingNumber.Protocol.ToMeetingType();
 
+			// Get the intersection of the supported conference source types against the booking source types
+			eConferenceSourceType supported = preferredDialer.Supports;
+			eConferenceSourceType meetingSourceType = GetConferenceSourceType(meetingType);
+			eConferenceSourceType intersection = EnumUtils.GetFlagsIntersection(supported, meetingSourceType);
 
-			if (preferredDialer.Supports.HasFlag(eConferenceSourceType.Audio))
+			// Convert back to meeting type
+			return GetMeetingType(intersection);
+		}
+
+		/// <summary>
+		/// Converts the conference source type to a meeting type.
+		/// </summary>
+		/// <param name="sourceType"></param>
+		/// <returns></returns>
+		private static eMeetingType GetMeetingType(eConferenceSourceType sourceType)
+		{
+			if (sourceType.HasFlag(eConferenceSourceType.Video))
+				return eMeetingType.VideoConference;
+			if (sourceType.HasFlag(eConferenceSourceType.Audio))
 				return eMeetingType.AudioConference;
 
 			return eMeetingType.Presentation;
@@ -101,27 +110,24 @@ namespace ICD.Connect.Conferencing.Utils
 			              .FirstOrDefault();
 		}
 
-		/// <summary>
-		/// Gets the meeting type for the given booking protocol.
-		/// </summary>
-		/// <param name="protocol"></param>
-		/// <returns></returns>
-		public static eMeetingType ToConferenceSourceType(eBookingProtocol protocol)
+		public static eConferenceSourceType GetConferenceSourceType(eBookingProtocol protocol)
 		{
-			switch (protocol)
+			return GetConferenceSourceType(protocol.ToMeetingType());
+		}
+
+		public static eConferenceSourceType GetConferenceSourceType(eMeetingType meetingType)
+		{
+			switch (meetingType)
 			{
-				case eBookingProtocol.Zoom:
-				case eBookingProtocol.Sip:
-					return eMeetingType.VideoConference;
-
-				case eBookingProtocol.Pstn:
-					return eMeetingType.AudioConference;
-
-				case eBookingProtocol.None:
-					return eMeetingType.Presentation;
+				case eMeetingType.AudioConference:
+					return eConferenceSourceType.Audio;
+				case eMeetingType.VideoConference:
+					return eConferenceSourceType.Audio | eConferenceSourceType.Video;
+				case eMeetingType.Presentation:
+					return eConferenceSourceType.Unknown;
 
 				default:
-					throw new ArgumentOutOfRangeException();
+					throw new ArgumentOutOfRangeException("meetingType");
 			}
 		}
 	}
