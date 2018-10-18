de7fe2eeb7adf9da620a132423b1f67ad8a79338
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-18 17:00:46 -0400

    refactor: Removing redundant method

--- ICD.Connect.Conferencing.Server/Devices/Client/InterpretationClientDevice.cs
+++ ICD.Connect.Conferencing.Server/Devices/Client/InterpretationClientDevice.cs
@@ -325,20 +325,6 @@ namespace ICD.Connect.Conferencing.Server.Devices.Client
 
 		#region Private Helper Methods
 
-		/// <summary>
-		/// Logs to logging core.
-		/// </summary>
-		/// <param name="severity"></param>
-		/// <param name="message"></param>
-		/// <param name="args"></param>
-		private void Log(eSeverity severity, string message, params object[] args)
-		{
-			message = string.Format(message, args);
-			message = string.Format("{0} - {1}", GetType().Name, message);
-
-			Logger.AddEntry(severity, message);
-		}
-
 		private void ClearSources()
 		{
 			m_SourcesCriticalSection.Enter();
