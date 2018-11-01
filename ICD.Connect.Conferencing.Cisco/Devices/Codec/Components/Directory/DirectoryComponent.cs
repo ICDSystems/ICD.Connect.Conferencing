3e0d088967003f4b8dd07badcb385eb8473029c5
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2018-11-01 15:00:52 -0400

    chore: revert sample cisco contact format

--- ICD.Connect.Conferencing.Cisco/Devices/Codec/Components/Directory/DirectoryComponent.cs
+++ ICD.Connect.Conferencing.Cisco/Devices/Codec/Components/Directory/DirectoryComponent.cs
@@ -385,7 +385,7 @@ namespace ICD.Connect.Conferencing.Cisco.Devices.Codec.Components.Directory
 			//     <ContactMethod item="1">
 			//       <ContactMethodId item="1">1</ContactMethodId>
 			//       <Number item="1">111</Number>
-			//       <CiscoCallType item="1">Video</CiscoCallType>
+			//       <CallType item="1">Video</CallType>
 			//     </ContactMethod>
 			//   </Contact>
 			// </PhonebookSearchResult>
