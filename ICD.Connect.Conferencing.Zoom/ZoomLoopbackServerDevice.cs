970a1f30dbf398c6cbeab1d9b83c39555028a508
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-02 16:36:27 -0400

    feat: Initial commit of ZoomLoopbackServerDevice and settings

---
+++ ICD.Connect.Conferencing.Zoom/ZoomLoopbackServerDevice.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.Properties;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Conferencing.Zoom.Responses;
+using ICD.Connect.Devices;
+using ICD.Connect.Protocol;
+using ICD.Connect.Protocol.Extensions;
+using ICD.Connect.Protocol.Network.Ports;
+using ICD.Connect.Protocol.Network.Settings;
+using ICD.Connect.Protocol.Ports;
+using ICD.Connect.Protocol.SerialBuffers;
+using ICD.Connect.Settings;
+using Newtonsoft.Json;
+
+namespace ICD.Connect.Conferencing.Zoom
+{
+	public sealed class ZoomLoopbackServerDevice : AbstractDevice<ZoomLoopbackServerSettings>
+	{
+		/// <summary>
+		/// End of line character(s) for ZR-CSAPI commands.
+		/// Must be \r, since the API doesn't accept the "format json" command otherwise.
+		/// </summary>
+		private const string END_OF_LINE = "\r";
+
+		private readonly ConnectionStateManager m_ConnectionStateManager;
+		private readonly JsonSerialBuffer m_SerialBuffer;
+
+		private readonly SecureNetworkProperties m_NetworkProperties;
+
+		#region Properties
+
+		/// <summary>
+		/// Device Initialized Status.
+		/// </summary>
+		public bool Initialized { get; private set; }
+
+		/// <summary>
+		/// Returns true when the codec is connected.
+		/// </summary>
+		public bool IsConnected
+		{
+			get { return m_ConnectionStateManager.IsConnected; }
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public ZoomLoopbackServerDevice()
+		{
+			m_NetworkProperties = new SecureNetworkProperties();
+
+			m_ConnectionStateManager = new ConnectionStateManager(this) {ConfigurePort = ConfigurePort};
+			Subscribe(m_ConnectionStateManager);
+
+			m_SerialBuffer = new JsonSerialBuffer();
+			Subscribe(m_SerialBuffer);
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void DisposeFinal(bool disposing)
+		{
+			base.DisposeFinal(disposing);
+
+			Unsubscribe(m_SerialBuffer);
+			Unsubscribe(m_ConnectionStateManager);
+		}
+
+		#region Methods
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+			return m_ConnectionStateManager.IsOnline;
+		}
+
+		/// <summary>
+		/// Sets the port for communicating with the device.
+		/// </summary>
+		/// <param name="port"></param>
+		[PublicAPI]
+		public void SetPort(ISerialPort port)
+		{
+			m_ConnectionStateManager.SetPort(port);
+		}
+
+		/// <summary>
+		/// Configures the given port for communication with the device.
+		/// </summary>
+		/// <param name="port"></param>
+		private void ConfigurePort(ISerialPort port)
+		{
+			// SSH
+			if (port is ISecureNetworkPort)
+				(port as ISecureNetworkPort).ApplyDeviceConfiguration(m_NetworkProperties);
+		}
+
+		/// <summary>
+		/// Send command.
+		/// </summary>
+		/// <param name="command"></param>
+		public void SendCommand(string command)
+		{
+			m_ConnectionStateManager.Send(command + END_OF_LINE);
+		}
+
+		/// <summary>
+		/// Sends commands.
+		/// </summary>
+		/// <param name="commands"></param>
+		[PublicAPI]
+		public void SendCommands(params string[] commands)
+		{
+			if (commands == null)
+				throw new ArgumentNullException("commands");
+
+			foreach (string command in commands)
+				SendCommand(command);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Initialize the Zoom Room API.
+		/// </summary>
+		private void Initialize()
+		{
+			m_ConnectionStateManager.Send("echo off");
+			m_ConnectionStateManager.Send("\r");
+			m_ConnectionStateManager.Send("format json");
+			m_ConnectionStateManager.Send("\r");
+		}
+
+		#endregion
+
+		#region Port Callbacks
+
+		/// <summary>
+		/// Subscribes to the port events.
+		/// </summary>
+		/// <param name="port"></param>
+		private void Subscribe(ConnectionStateManager port)
+		{
+			port.OnSerialDataReceived += PortOnSerialDataReceived;
+			port.OnConnectedStateChanged += PortOnConnectionStatusChanged;
+			port.OnIsOnlineStateChanged += PortOnIsOnlineStateChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the port events.
+		/// </summary>
+		/// <param name="port"></param>
+		private void Unsubscribe(ConnectionStateManager port)
+		{
+			port.OnSerialDataReceived -= PortOnSerialDataReceived;
+			port.OnConnectedStateChanged -= PortOnConnectionStatusChanged;
+			port.OnIsOnlineStateChanged -= PortOnIsOnlineStateChanged;
+		}
+
+		/// <summary>
+		/// Called when serial data is recieved from the port.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void PortOnSerialDataReceived(object sender, StringEventArgs args)
+		{
+			if (args.Data.StartsWith("{"))
+			{
+				SerialBufferCompletedSerial(sender, args);
+				Initialized = true;
+			}
+			else if (args.Data.Contains("Login") || args.Data.StartsWith("*"))
+				Initialize();
+		}
+
+		/// <summary>
+		/// Called when the port connection status changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void PortOnConnectionStatusChanged(object sender, BoolEventArgs args)
+		{
+			m_SerialBuffer.Clear();
+
+			if (!args.Data)
+			{
+				Log(eSeverity.Critical, "Lost connection");
+				Initialized = false;
+			}
+		}
+
+		/// <summary>
+		/// Called when the port online status changes.1
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void PortOnIsOnlineStateChanged(object sender, BoolEventArgs args)
+		{
+			UpdateCachedOnlineStatus();
+		}
+
+		#endregion
+
+		#region Buffer Callbacks
+
+		/// <summary>
+		/// Subscribes to the buffer events.
+		/// </summary>
+		/// <param name="buffer"></param>
+		private void Subscribe(ISerialBuffer buffer)
+		{
+			buffer.OnCompletedSerial += SerialBufferCompletedSerial;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the buffer events.
+		/// </summary>
+		/// <param name="buffer"></param>
+		private void Unsubscribe(ISerialBuffer buffer)
+		{
+			buffer.OnCompletedSerial -= SerialBufferCompletedSerial;
+		}
+
+		/// <summary>
+		/// Called when the buffer completes a string.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void SerialBufferCompletedSerial(object sender, StringEventArgs args)
+		{
+			string json = args.Data;
+
+			AbstractZoomRoomResponse response = DeserializeResponse(json);
+			if (response == null)
+				return;
+
+			// TODO
+			//CallResponseCallbacks(response);
+
+			Initialized = true;
+		}
+
+		[CanBeNull]
+		private AbstractZoomRoomResponse DeserializeResponse(string data)
+		{
+			AttributeKey key;
+			if (!AttributeKey.TryParse(data, out key))
+				return null;
+
+			AbstractZoomRoomResponse response = null;
+
+			try
+			{
+				// Find concrete type that matches the json values
+				Type responseType = key.GetResponseType();
+				if (responseType != null)
+					response = JsonConvert.DeserializeObject(data, responseType) as AbstractZoomRoomResponse;
+			}
+			catch (Exception ex)
+			{
+				// Zoom gives us bad json (unescaped characters) in some error messages
+				Log(eSeverity.Error, ex, "Failed to deserialize JSON");
+			}
+
+			return response;
+		}
+
+		#endregion
+
+		#region Settings
+
+		protected override void ApplySettingsFinal(ZoomLoopbackServerSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			m_NetworkProperties.Copy(settings);
+
+			ISerialPort port = null;
+
+			if (settings.Port != null)
+			{
+				try
+				{
+					port = factory.GetPortById((int)settings.Port) as ISerialPort;
+				}
+				catch (KeyNotFoundException)
+				{
+					Log(eSeverity.Error, "No serial port with id {0}", settings.Port);
+				}
+			}
+
+			SetPort(port);
+		}
+
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			m_NetworkProperties.ClearNetworkProperties();
+
+			SetPort(null);
+		}
+
+		protected override void CopySettingsFinal(ZoomLoopbackServerSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.Port = m_ConnectionStateManager.PortNumber;
+
+			settings.Copy(m_NetworkProperties);
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the help information for the node.
+		/// </summary>
+		public override string ConsoleHelp { get { return "The Zoom Loopback Server device"; } }
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			addRow("Initialized", Initialized);
+			addRow("IsConnected", IsConnected);
+		}
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Conferencing.Zoom/ZoomLoopbackServerSettings.cs
@@ -0,0 +1,119 @@
+﻿using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices;
+using ICD.Connect.Protocol.Network.Settings;
+using ICD.Connect.Protocol.Ports;
+using ICD.Connect.Settings.Attributes;
+using ICD.Connect.Settings.Attributes.SettingsProperties;
+
+namespace ICD.Connect.Conferencing.Zoom
+{
+	[KrangSettings("ZoomLoopbackServer", typeof(ZoomLoopbackServerDevice))]
+	public sealed class ZoomLoopbackServerSettings : AbstractDeviceSettings, ISecureNetworkSettings
+	{
+		private const string PORT_ELEMENT = "Port";
+
+		private readonly SecureNetworkProperties m_NetworkProperties;
+
+		#region Properties
+
+		/// <summary>
+		/// The port id.
+		/// </summary>
+		[OriginatorIdSettingsProperty(typeof(ISerialPort))]
+		public int? Port { get; set; }
+
+		#endregion
+
+		#region Network
+
+		/// <summary>
+		/// Gets/sets the configurable network username.
+		/// </summary>
+		public string NetworkUsername
+		{
+			get { return m_NetworkProperties.NetworkUsername; }
+			set { m_NetworkProperties.NetworkUsername = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the configurable network password.
+		/// </summary>
+		public string NetworkPassword
+		{
+			get { return m_NetworkProperties.NetworkPassword; }
+			set { m_NetworkProperties.NetworkPassword = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the configurable network address.
+		/// </summary>
+		public string NetworkAddress
+		{
+			get { return m_NetworkProperties.NetworkAddress; }
+			set { m_NetworkProperties.NetworkAddress = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the configurable network port.
+		/// </summary>
+		public ushort? NetworkPort
+		{
+			get { return m_NetworkProperties.NetworkPort; }
+			set { m_NetworkProperties.NetworkPort = value; }
+		}
+
+		/// <summary>
+		/// Clears the configured values.
+		/// </summary>
+		void INetworkProperties.ClearNetworkProperties()
+		{
+			m_NetworkProperties.ClearNetworkProperties();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public ZoomLoopbackServerSettings()
+		{
+			m_NetworkProperties = new SecureNetworkProperties();
+		}
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(PORT_ELEMENT, Port == null ? null : IcdXmlConvert.ToString((int) Port));
+
+			m_NetworkProperties.WriteElements(writer);
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			Port = XmlUtils.TryReadChildElementContentAsInt(xml, PORT_ELEMENT);
+
+			m_NetworkProperties.ParseXml(xml);
+
+			UpdateNetworkDefaults();
+		}
+
+		/// <summary>
+		/// Sets default values for unconfigured network properties.
+		/// </summary>
+		private void UpdateNetworkDefaults()
+		{
+			m_NetworkProperties.ApplyDefaultValues(null, 2244, "zoom", "zoomus123");
+		}
+	}
+}
