b1224a9f2a8a2be7c9fb736ea3793aea767c36d9
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-16 17:28:17 -0400

    feat: Zoom Middleware installation adds a firewall exception and starts the service

--- ICD.Connect.Conferencing.ZoomMiddleware/Install.bat
+++ ICD.Connect.Conferencing.ZoomMiddleware/Install.bat
@@ -4,6 +4,7 @@ set InstallPath="C:\Program Files\ICD Systems\ZoomMiddleware\"
 set Application="ICD.Connect.Conferencing.ZoomMiddleware.exe"
 set LocalApplicationPath=%~dp0%Application%
 set TargetApplicationPath=%InstallPath%%Application%
+set ServiceName="ICD Zoom Middleware Service"
 
 set ZoomUsername="zoom"
 set ZoomPassword="zoomus123"
@@ -13,6 +14,8 @@ set ListenPort=2245
 
 %LocalApplicationPath% uninstall --sudo
 rmdir /s /q %InstallPath%
+
+netsh advfirewall firewall delete rule name=%ServiceName%
 
 set /p ZoomUsername=Zoom Username:
 set /p ZoomPassword=Zoom Password:
@@ -25,6 +28,11 @@ copy %~dp0\* %InstallPath%
 
 %TargetApplicationPath% install --sudo -zoomUsername=%ZoomUsername% -zoomPassword=%ZoomPassword% -zoomPort=%ZoomPort% -listenAddress=%ListenAddress% -listenPort=%ListenPort%
 
-sc start "ICD Zoom Middleware Service"
+netsh advfirewall firewall show rule name=%ServiceName% >nul
+if ERRORLEVEL 1 (
+    netsh advfirewall firewall add rule name=%ServiceName% dir=in action=allow protocol=TCP localport=%ListenPort% service=%ServiceName%
+)
+
+sc start %ServiceName%
 
 PAUSE
--- ICD.Connect.Conferencing.ZoomMiddleware/Uninstall.bat
+++ ICD.Connect.Conferencing.ZoomMiddleware/Uninstall.bat
@@ -3,8 +3,11 @@
 set InstallPath="C:\Program Files\ICD Systems\ZoomMiddleware\"
 set Application="ICD.Connect.Conferencing.ZoomMiddleware.exe"
 set LocalApplicationPath=%~dp0%Application%
+set ServiceName="ICD Zoom Middleware Service"
 
 %LocalApplicationPath% uninstall --sudo
 rmdir /s /q %InstallPath%
+
+netsh advfirewall firewall delete rule name=%ServiceName%
 
 PAUSE
