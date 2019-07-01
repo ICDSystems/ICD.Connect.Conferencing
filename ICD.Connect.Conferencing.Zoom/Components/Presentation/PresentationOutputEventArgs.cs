970c7f6dc72cb6076ff7f1bd4ff56f8556f3d43e
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2019-07-01 15:49:58 -0400

    feat: update ZoomRoomPresentationControl

---
+++ ICD.Connect.Conferencing.Zoom/Components/Presentation/PresentationOutputEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using ICD.Common.Utils.EventArguments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICD.Connect.Conferencing.Zoom.Components.Presentation
+{
+	public class PresentationOutputEventArgs : GenericEventArgs<int?>
+	{
+		public PresentationOutputEventArgs(int? data) : base(data)
+		{
+		}
+	}
+}
