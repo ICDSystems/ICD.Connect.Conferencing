7b1f399ed7ed1b7cd4961ad9247cd671a2259f52
Chris Cameron <chris.cameron@wedoresi.com>
2019-07-16 17:15:12 -0400

    chore: Updating changelog, incrementing patch version

--- ICD.Connect.Conferencing.Polycom/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing.Polycom/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing.Polycom")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("13.3.0.0")]
+[assembly: AssemblyVersion("13.3.1.0")]
