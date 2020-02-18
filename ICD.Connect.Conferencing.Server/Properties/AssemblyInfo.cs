d77a30d131a78884c619e094afa0a4294d2efef5
Chris Cameron <chris.cameron@wedoresi.com>
2020-02-18 15:49:14 -0500

    chore: Updating changelog, incrementing patch version

--- ICD.Connect.Conferencing.Server/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing.Server/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing.Server")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("11.9.0.0")]
+[assembly: AssemblyVersion("11.9.1.0")]
