4227c78efe7eda684004a68878f410ee560d1e69
Austin Noska <austin.noska@profound-tech.com>
2020-06-11 17:19:55 -0400

    feat: Custom serial buffer for the AV Bridge with NUnit Tests

---
+++ ICD.Connect.Conferencing.Vaddio.Tests/ICD.Connect.Conferencing.Vaddio.Tests/Devices/AvBridge/VaddioAvBridgeSerialBufferTest.cs
@@ -0,0 +1,64 @@
+﻿using ICD.Common.Utils;
+using ICD.Connect.Conferencing.Vaddio.Devices.AvBridge;
+using NUnit.Framework;
+
+namespace ICD.Connect.Conferencing.Vaddio.Tests.Devices.AvBridge
+{
+	[TestFixture]
+	public sealed class VaddioAvBridgeSerialBufferTest
+	{
+		public sealed class VaddioAvBridgeSerialBufferProcessDelimterTestData
+		{
+			public string Data { get; set; }
+			public string Expected { get; set; }
+		}
+
+		private static readonly VaddioAvBridgeSerialBufferProcessDelimterTestData[] s_TestData =
+		{
+			// Standard response.
+			new VaddioAvBridgeSerialBufferProcessDelimterTestData
+			{
+				Data = "audio mute on\r\nOK\r\n>\r\n",
+				Expected = "audio mute on\r\nOK\r\n"
+			},
+
+			// Response with another delimiter ':' in the middle of the response.
+			new VaddioAvBridgeSerialBufferProcessDelimterTestData
+			{
+				Data = "audio volume get\r\nvolume: 10\r\nOK\r\n>\r\n",
+				Expected = "audio volume get\r\nvolume: 10\r\nOK\r\n"
+			},
+
+			// Error response.
+			new VaddioAvBridgeSerialBufferProcessDelimterTestData
+			{
+				Data = "gibberish\r\nSyntax error: Unknown or incomplete command\r\n>",
+				Expected = "gibberish\r\nSyntax error: Unknown or incomplete command\r\n"
+			},
+
+			// Login prompt followed by other response data.
+			new VaddioAvBridgeSerialBufferProcessDelimterTestData
+			{
+				Data = "login:\r\nadmin\r\nPassword:\r\n\r\n>audio mute on\r\nOK\r\n>\r\n",
+				Expected = "audio mute on\r\nOK\r\n"
+			}
+		};
+
+
+		[Test]
+		public void ProcessDelimterTest(
+			[ValueSource(nameof(s_TestData))] VaddioAvBridgeSerialBufferProcessDelimterTestData testData)
+		{
+			string processed = null;
+
+			VaddioAvBridgeSerialBuffer buffer = new VaddioAvBridgeSerialBuffer();
+			buffer.OnCompletedSerial += (sender, args) => processed = args.Data;
+
+			buffer.Enqueue(testData.Data);
+
+			ThreadingUtils.Sleep(3000);
+
+			Assert.AreEqual(testData.Expected, processed);
+		}
+	}
+}
---
+++ ICD.Connect.Conferencing.Vaddio/Devices/AvBridge/VaddioAvBridgeSerialBuffer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.Protocol.SerialBuffers;
+
+namespace ICD.Connect.Conferencing.Vaddio.Devices.AvBridge
+{
+	public sealed class VaddioAvBridgeSerialBuffer : AbstractSerialBuffer
+	{
+		private static readonly char[] s_Delimiters =
+		{
+			'>',
+			':',
+		};
+
+		/// <summary>
+		/// Raised when a username prompt has been buffered.
+		/// </summary>
+		public event EventHandler OnUsernamePrompt;
+
+		/// <summary>
+		/// Raised when a password prompt has been buffered.
+		/// </summary>
+		public event EventHandler OnPasswordPrompt;
+
+		private readonly StringBuilder m_RxData;
+
+		public VaddioAvBridgeSerialBuffer()
+		{
+			m_RxData = new StringBuilder();
+		}
+
+		/// <summary>
+		/// Override to clear any current state.
+		/// </summary>
+		protected override void ClearFinal()
+		{
+			m_RxData.Clear();
+		}
+
+		/// <summary>
+		/// Override to process the given item for chunking.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		protected override IEnumerable<string> Process(string data)
+		{
+			while (true)
+			{
+				int index = data.IndexOfAny(s_Delimiters);
+				if (index < 0)
+				{
+					m_RxData.Append(data);
+					break;
+				}
+
+				char delimiter = data[index];
+
+				m_RxData.Append(data.Substring(0, index));
+				data = data.Substring(index + 1);
+
+				switch (delimiter)
+				{
+					// Login prompt
+					case ':':
+						string prompt = m_RxData.ToString().ToLower();
+
+						if (prompt.Contains("login"))
+						{
+							m_RxData.Clear();
+							OnUsernamePrompt.Raise(this);
+							break;
+						}
+						else if (prompt.Contains("password"))
+						{
+							m_RxData.Clear();
+							OnPasswordPrompt.Raise(this);
+							break;
+						}
+						// Unhandled colon, push it back onto the rx data
+						else
+						{
+							m_RxData.Append(':');
+							continue;
+						}
+				}
+
+				string output = m_RxData.Pop();
+				if (!string.IsNullOrEmpty(output))
+					yield return output;
+			}
+		}
+	}
+}
