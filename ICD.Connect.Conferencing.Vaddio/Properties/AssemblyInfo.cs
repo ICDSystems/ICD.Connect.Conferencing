3e98c8a2e712c5be67b0c5bba83ccfc457346271
Chris Cameron <chris.cameron@wedoresi.com>
2021-01-06 16:45:04 -0500

    chore: Updating copyright year

--- ICD.Connect.Conferencing.Vaddio/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing.Vaddio/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Conferencing.Vaddio")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing.Vaddio")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("16.0.0.0")]
--- ICD.Connect.Conferencing.Zoom/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing.Zoom/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Conferencing.Zoom")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing.Zoom")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("17.1.2.0")]
