64a3e872d2e276ee463265465bd4d9999dfb6c68
Drew Tingen <drew.tingen@icdsystems.com>
2021-01-15 00:24:06 -0500

    chore: update changelog, increment assembly version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [17.2.0] - 2021-01-14
 ### Added
  - Vaddio AV Bridge device driver
  - Added a ContentInput property for VideoConferenceRoutControls
--- ICD.Connect.Conferencing.Zoom/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing.Zoom/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing.Zoom")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
-[assembly: AssemblyVersion("17.1.2.0")]
+[assembly: AssemblyVersion("17.2.0.0")]
