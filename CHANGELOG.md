2371c58e9b4eed60fbe896d18c9ac47fb5c4fc48
Drew Tingen <drew.tingen@icdsystems.com>
2021-08-03 11:16:43 -0400

    chore: Update Changelog, increment major version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [19.0.0] - 2021-08-03
 ### Added
  - Direct Sharing Control interfaces & abstractions
  - CiscoCodecDirectSharingControl to support Cisco Proximity sharing
--- ICD.Connect.Conferencing/Properties/AssemblyInfo.cs
+++ ICD.Connect.Conferencing/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Conferencing")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
-[assembly: AssemblyVersion("18.0.0.0")]
+[assembly: AssemblyVersion("19.0.0.0")]
