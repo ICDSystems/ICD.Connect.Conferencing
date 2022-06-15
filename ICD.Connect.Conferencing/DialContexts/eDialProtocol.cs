8e0df64659d2e38a973439d5e7690dde84db4b1a
Drew Tingen <drew.tingen@icdsystems.com>
2022-06-15 18:36:46 -0400

    feat: eDialProtocol to eCallType extension

--- ICD.Connect.Conferencing/DialContexts/eDialProtocol.cs
+++ ICD.Connect.Conferencing/DialContexts/eDialProtocol.cs
@@ -1,4 +1,7 @@
-﻿namespace ICD.Connect.Conferencing.DialContexts
+﻿using System;
+using ICD.Connect.Conferencing.EventArguments;
+
+namespace ICD.Connect.Conferencing.DialContexts
 {
 	/// <summary>
 	/// The types of protocols for booking numbers.
@@ -13,5 +16,37 @@
 		ZoomContact = 4,
 		ZoomPersonal = 5,
 		Spark = 6
+	}
+
+	public static class DialProtocolExtensions
+	{
+		/// <summary>
+		/// Gets the most appropriate call type for the given dial protocol
+		/// </summary>
+		/// <remakrs>
+		/// This is not guaranteed to be correct, as many protocols can be used for both audio and video.
+		/// Use with caution!
+		/// </remakrs>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static eCallType GetCallType(this eDialProtocol extends)
+		{
+			switch (extends)
+			{
+				case eDialProtocol.Unknown:
+					return eCallType.Unknown;
+				case eDialProtocol.Pstn:
+					return eCallType.Audio;
+				case eDialProtocol.Sip:
+				case eDialProtocol.Zoom:
+				case eDialProtocol.ZoomContact:
+				case eDialProtocol.ZoomPersonal:
+				case eDialProtocol.Spark:
+					return eCallType.Video;
+					
+				default:
+					throw new ArgumentOutOfRangeException("extends");
+			}
+		}
 	}
 }
